0cd19264055704199acd7abb275ad58c51fff5de
Björn Ritzl <bjorn.ritzl@gmail.com>
2020-08-24 09:08:57 +0200

    Cleaned up siwa header

--- siwa/src/siwa.h
+++ siwa/src/siwa.h
@@ -48,12 +48,6 @@ struct SiwaCallbackData
 	char* m_message;
 };
 
-// Data used to ensure we only have 1 request in progress with Apples servers at a time.
-// Access should be restricted behind mutex protection as the callbacks from apple can
-// be triggered in arbitrary contexts.
-// This data is also used to hold information passed from lua until the callback comes
-// back from Apple, so that we may pass it back to lua to identify the request we are
-// getting a callback for.
 struct SiwaData
 {
     SiwaData()
@@ -69,20 +63,12 @@ struct SiwaData
 
 char* Siwa_GetUserId();
 
-void Siwa_CheckForQueuedCallbacks();
-void Siwa_InitCallbackData();
-void Siwa_ResetCallbackData();
-
 // Queue the credential check callback to be triggered next update call in the main thread.
 void Siwa_QueueCredentialCallback(const char* userID, const SiwaCredentialState state);
 // Queue the sign in authorization callback to be triggered next update call in the main thread, when authorization succeeds.
 void Siwa_QueueAuthSuccessCallback(const char* identityToken, const char* userID, const char* email, const char* firstName, const char* familyName, const SiwaUserDetectionStatus userStatus);
 // Queue the sign in authorization callback to be triggered next update call in the main thread, when authorization fails.
 void Siwa_QueueAuthFailureCallback(const char* message);
-
-void Siwa_TriggerCallback();
-void Siwa_SetupCallback(lua_State* L, int index);
-void Siwa_CleanupCallback();
 
 // Trigged by a call from lua to check if sign in with apple is supported on this device.
 bool Siwa_PlatformIsSupported();
